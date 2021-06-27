e343531e7a8b29cadea6ca63598009e4eba21bce
Jean Garcia <jean_clg@hotmail.com>
2021-06-27 18:13:06 -0300

    Log list of news begin server

--- index.js
+++ index.js
@@ -10,8 +10,10 @@ const feeds = [];
 
 (async () => {
   let feed = await parser.parseURL("http://g1.globo.com/dynamo/rss2.xml");
-  feeds.push(feed.items[feed.items.length - 1].title);
-  console.log({Title: feeds[0], Link: feed.items[feed.items.length - 1].link});
+  feed.items.forEach(item => {
+    feeds.push(item.title);
+    console.log({Title: item.title, Link: item.link});
+  });
   async function loop(rss) {
     feed = await parser.parseURL("http://g1.globo.com/dynamo/rss2.xml");
     if (!feeds.includes(rss[rss.length - 1].title)) {
